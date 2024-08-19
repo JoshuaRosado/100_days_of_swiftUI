a14ab15eb40fc6a528642e6da16dc16ecd28ad79
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2024-08-18 20:31:26 -0500

    day-8, how to set default values for parameters

---
+++ day-8/errors_in_func/errors_in_func.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+import Foundation
+
+// HOW TO HANDLE ERRORS IN FUNCTIONS
+// 3 STEPS TO HANDLE ERROR IN FUNCTIONS
+
+// ---- step 1
+// Define all the errors that might happen in the code we are writing
+
+// ----- step 2
+// Write a function that works as normal, but that can throw an error if a serious problem is found
+
+// ----- step 3
+// Run that function and handle every problem that is found
+
+
