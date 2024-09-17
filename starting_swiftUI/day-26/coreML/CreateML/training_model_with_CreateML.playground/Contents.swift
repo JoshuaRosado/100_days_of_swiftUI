97616a6e6930c82dce2d419a184cb1fbd6785813
JoshuaRosado <joshua06rosado@gmail.com>
2024-09-17 10:33:26 -0500

    CREATE ML training model in process

---
+++ starting_swiftUI/day-26/coreML/CreateML/training_model_with_CreateML.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+import Foundation
+
+
+// TRAINING A MODEL WITH CREATE ML
+
+
+// ==========================================================
+
+
+// CORE ML = let's us use machine learning models in our app to make predictions
+
+// CREATE ML = let's us create custom machine learning models with our own data
+
+
+// Tabular Regression = Throwing a whole bunch of spread sheets data at CREATE ML and ask it to figure out the different relationship of various values for us
+
+
+// ML 2 STEPS
+
+// Step 1 : We train the model with CREATE ML
+// Step 2 : Ask model to make prediction with CORE ML
+
+// ==========================================================
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+        
+    }
+}
+#Preview {
+    ContentView()
+}
+
