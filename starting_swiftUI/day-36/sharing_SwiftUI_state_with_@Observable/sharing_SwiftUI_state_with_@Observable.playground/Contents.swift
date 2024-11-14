db114b2b5ef5e60cb1de56772df3882129b83e15
JoshuaRosado <joshua06rosado@gmail.com>
2024-11-14 15:21:20 -0600

    Sharing SwiftUI state with @Observable

--- starting_swiftUI/day-36/sharing_SwiftUI_state_with_@Observable/sharing_SwiftUI_state_with_@Observable.playground/Contents.swift
+++ starting_swiftUI/day-36/sharing_SwiftUI_state_with_@Observable/sharing_SwiftUI_state_with_@Observable.playground/Contents.swift
@@ -1,12 +1,15 @@
 import UIKit
 
 
+// Sharing SwiftUI state with @Observable
 
+import Observation
 import SwiftUI
 
 
+//If you use @State with a struct, your SwiftUI view will update automatically when a value changes, but if you use @State with a class then you must mark that class with @Observable if you want SwiftUI to watch its contents for changes.
 
-@Observable // EXPLANATION in next Chapter
+@Observable // = Tells SwiftUI to Look at the properties inside the View and reload any View that relie s in a property when it changes
 
 class User {
     var firstName = "Bilbo"
