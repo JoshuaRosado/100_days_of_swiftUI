e0bfd1120a4649ef5222ab29bb3b12f7eb70700b
JoshuaRosado <joshua06rosado@gmail.com>
2024-11-15 13:55:06 -0600

    Deleting items using onDelete()

---
+++ starting_swiftUI/day-36/deleting_items_using_onDelete()/deleting_items_using_onDelete().playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+
+// Deleting items using onDelete()
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    var body: some View {
+        NavigationStack{ // Using NavigationStack to implement edit button for deleting
+            
+            
+            VStack {
+                List{
+                    ForEach(numbers, id:\.self){
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                Button("Add number"){// add a number to the empty list
+                    numbers.append(currentNumber)
+                    currentNumber += 1 // increment number of row
+                }
+                
+            }
+            // adding Edit button for delete
+            .toolbar{
+                EditButton()
+        }
+        
+        }
+    }
+    // remove IndexSet ( Int selected "Position" ) from the array
+    func removeRows(at offsets: IndexSet){
+        numbers.remove(atOffsets: offsets)
+    }
+}
+
+#Preview {
+    ContentView()
+}
