c9995b05c28d78314d83cccb7b32d6834c355207
JoshuaRosado <joshua06rosado@gmail.com>
2024-11-28 22:25:26 -0600

    different ways to resize an image

---
+++ starting_swiftUI/day-39/resizing_imgs_to_fit_the_available_space/resizing_imgs_to_fit_available_space.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+//        Image("example")
+        Image(.example)
+            .resizable() // resize the image
+            .scaledToFit() // scaled to FIT the parent
+            .scaledToFill() // scaled to FILL parent
+            
+        // make this image 80% of the available width
+            .containerRelativeFrame(.horizontal){
+                size, axis in size * 0.8
+            }
+        
+        // make this image 90% of the available height
+            .containerRelativeFrame(.vertical){
+                size, axis in size * 0.9
+            }
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
