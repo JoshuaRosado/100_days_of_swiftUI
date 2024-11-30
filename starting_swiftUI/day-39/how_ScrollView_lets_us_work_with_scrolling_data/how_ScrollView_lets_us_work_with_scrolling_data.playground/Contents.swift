a2472f1cde450396b645470b224d28d72ac9a0b8
JoshuaRosado <joshua06rosado@gmail.com>
2024-11-29 19:47:30 -0600

    ScrollView and scrolling data

---
+++ starting_swiftUI/day-39/how_ScrollView_lets_us_work_with_scrolling_data/how_ScrollView_lets_us_work_with_scrolling_data.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+// How ScrollView lets us work with scrolling data
+
+
+import SwiftUI
+
+struct CustomText: View{
+    var text: String
+    
+    var body: some View{
+        
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Custom Text was created \(text)")
+        self.text = text
+    }
+}
+
+// VStack will create a text all at once, when the app is showing
+// VStack takes the space needed
+
+// LazyVStack will create a text when in demand. When item is shown
+// LazyVStack takes all the space
+
+
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView(.horizontal){
+            LazyHStack(spacing: 10){
+                ForEach(0..<100){
+                    CustomText(text:"Item \($0)")
+                        .font(.title)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
