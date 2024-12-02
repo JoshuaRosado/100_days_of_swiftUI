c263fe5d5bfab7cc854871d4123ac03d1bcd066c
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-01 21:35:51 -0600

    adding json files to the app

---
+++ starting_swiftUI/day-40/loading_a_specific_kind_of_Codable_data/loading_a_specific_kind_of_Codable_data.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// Loading a specific kind of Codable data
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
