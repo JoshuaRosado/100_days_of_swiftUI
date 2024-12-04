1d2c949fe72b1c912be0aecac052d493874c1372
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-03 21:44:32 -0600

    Loading specific kind of Codable data

--- starting_swiftUI/day-40/loading_a_specific_kind_of_Codable_data/loading_a_specific_kind_of_Codable_data.playground/Contents.swift
+++ starting_swiftUI/day-40/loading_a_specific_kind_of_Codable_data/loading_a_specific_kind_of_Codable_data.playground/Contents.swift
@@ -5,12 +5,64 @@ import UIKit
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        
-    }
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id : String
+    let name : String
+    let description: String
 }
 
-#Preview {
-    ContentView()
+
+
+
+extension Bundle {
+    // decode some file
+    // return String: Astronaut Dict
+    func decode(_ file: String) -> [String: Astronaut] {
+        // URL
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle. ")
+        }
+        
+        // finding data and loading it
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to locate \(file) from bundle. ")
+        }
+        // if we found the data and loaded
+        // decoded
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            // try to return the data decoded
+            return try decoder.decode([String: Astronaut].self, from: data)
+
+        // catch if there's a key missing from data
+            
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue) - \(context.debugDescription)" )
+            
+            // catch if there's a mismatch
+            
+        } catch DecodingError.typeMismatch(_, let context){
+            fatalError("Failed to decode \(file) from bundle due to type mismatch - \(context.debugDescription)")
+            
+            // catch if there's a missing value
+            
+        } catch DecodingError.valueNotFound(let type, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing \(type) value = \(context.debugDescription)")
+            
+            
+            // catch if the JSON file is invalid
+            
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed to decode \(file) from bundle because it appears o be invalid JSON")
+            
+        } catch {
+            fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
+        }
+        
+    }
 }
