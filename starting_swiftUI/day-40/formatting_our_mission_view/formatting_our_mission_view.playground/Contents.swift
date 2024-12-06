90a887dd487adf8190a653a8ad60b84c6a326d10
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-06 07:41:41 -0600

    Displaying logos with name and launchDates

---
+++ starting_swiftUI/day-40/formatting_our_mission_view/formatting_our_mission_view.playground/Contents.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+// Formatting out mission view
+
+import SwiftUI
+
+struct ContentView: View {
+    let astronauts : [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    let missions : [Mission] = Bundle.main.decode("missions.json")
+    
+    // create an ARRAY of adaptive columns with GridItem
+    // with a minimum but no max so it can expands more if its needed
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    var body: some View {
+        NavigationStack{
+            ScrollView{
+                LazyVGrid(columns: columns){
+                    ForEach(missions) { mission in
+                        NavigationLink{
+                            Text("Detail View")
+                        } label: {
+                            VStack{
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width:100, height:100)
+                                
+                                VStack{
+                                    Text(mission.displayName)
+                                    
+                                    Text(mission.launchDate ?? "N/A")
+                                        .font(.caption)
+                                    
+                                }
+                                .frame(width: .infinity)
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
