4d505de1311752524a628d02bc40824c2102b85e
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-14 20:58:17 -0600

    Intro to Navigation App

---
+++ starting_swiftUI/day-43/the_problem_with_a_simple_NavigationLink/the_problem_with_a_simple_NavigationLink.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+// A problem with a simple NavigationLink
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Navigation App")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
