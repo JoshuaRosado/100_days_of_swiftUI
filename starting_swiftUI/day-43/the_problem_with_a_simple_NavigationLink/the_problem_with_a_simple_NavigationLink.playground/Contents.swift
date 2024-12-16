52c68f76a8d217d335f43e10e5fbfe477bcacfb3
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-16 10:26:09 -0600

    Using Hashable and navigationDestination()

--- starting_swiftUI/day-43/the_problem_with_a_simple_NavigationLink/the_problem_with_a_simple_NavigationLink.playground/Contents.swift
+++ starting_swiftUI/day-43/the_problem_with_a_simple_NavigationLink/the_problem_with_a_simple_NavigationLink.playground/Contents.swift
@@ -5,33 +5,13 @@ import UIKit
 
 import SwiftUI
 
-struct DetailView: View {
-    
-    var number: Int
-    var body: some View {
-        Text("Detail View \(number)")
-    }
-    
-    init(number: Int) {
-        self.number = number
-        print("Creating detail view \(number)")
-    }
-}
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
-            List(0..<1000) { i in
-                NavigationLink("Tap Me") {
-                    DetailView(number: i)
-                }
-            }
-        }
-        
+        NavigationStack
     }
 }
 
 #Preview {
     ContentView()
 }
-
