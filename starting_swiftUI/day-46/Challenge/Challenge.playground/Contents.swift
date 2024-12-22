63c6ded21b75e805bb41532358a372a51ab25c86
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-21 19:31:00 -0600

    Challenge of day 46

---
+++ starting_swiftUI/day-46/Challenge/Challenge.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+//Challenge
+
+
+
+//One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try extending this app to make sure you fully understand what’s going on.
+//
+// 1 ---- Change project 7 (iExpense) so that it uses NavigationLink for adding new expenses rather than a sheet. (Tip: The dismiss() code works great here, but you might want to add the navigationBarBackButtonHidden() modifier so they have to explicitly choose Cancel.)
+
+// 2 ----  Try changing project 7 so that it lets users edit their issue name in the navigation title rather than a separate textfield. Which option do you prefer?
+
+// 3 ---- Return to project 8 (Moonshot), and upgrade it to use NavigationLink(value:). This means adding Hashable conformance, and thinking carefully how to use navigationDestination().
