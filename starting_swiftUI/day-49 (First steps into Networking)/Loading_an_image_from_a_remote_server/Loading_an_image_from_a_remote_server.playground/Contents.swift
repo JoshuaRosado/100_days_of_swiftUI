8537d9612cf1f09d31ff781e88286c9119bab991
JoshuaRosado <joshua06rosado@gmail.com>
2025-01-24 19:42:53 -0600

    Loading an image from a remote server

---
+++ starting_swiftUI/day-49 (First steps into Networking)/Loading_an_image_from_a_remote_server/Loading_an_image_from_a_remote_server.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+
+// Loading an image from a remote server
+
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            // OPTION 1
+            
+            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3)
+            
+            // OPTION 2
+            
+            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                ProgressView()
+            }
+                    .frame(width: 200, height: 200)
+            
+            
+            // OPTION 3
+            
+            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } else if phase.error != nil {
+                    Text("There was an error loading the image")
+                } else {
+                    ProgressView()
+                }
+            }
+                .frame(width: 200, height: 200)
+            
+            
+            }
+            
+        }
+        
+    }
+
+
+#Preview {
+    ContentView()
+}
