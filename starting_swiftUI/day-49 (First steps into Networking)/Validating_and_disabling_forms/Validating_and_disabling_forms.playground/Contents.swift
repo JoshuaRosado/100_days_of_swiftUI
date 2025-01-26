57901c1ce31bb871415ff68a86a6c3b408b9c4b2
JoshuaRosado <joshua06rosado@gmail.com>
2025-01-25 19:27:33 -0600

    Forms validation, disabled()

---
+++ starting_swiftUI/day-49 (First steps into Networking)/Validating_and_disabling_forms/Validating_and_disabling_forms.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+// Validating and disabling forms
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var username = ""
+    @State private var email = ""
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create account"){
+                    print("Creating account")
+                }
+                .foregroundStyle(.green)
+            }
+//            // Disable button if Field are not filled
+//            .disabled(username.isEmpty || email.isEmpty)
+            
+            // Disable button if Field's characters are less than 5
+            .disabled(disableForm)
+        }
+                
+    }
+    // if username or email are less than 5 character keep the button disabled
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+}
+
+#Preview {
+    ContentView()
+}
