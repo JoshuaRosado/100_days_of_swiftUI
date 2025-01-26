cb534e4e0e91aa5f094567a687753381ed0d3800
JoshuaRosado <joshua06rosado@gmail.com>
2025-01-25 19:44:05 -0600

    Codable conformance to an @Observable class

---
+++ starting_swiftUI/day-50( CupcakeCorner )/Adding_codable_conformance_to_an_@Observable_class/Adding_Codable_conformance_to_an_@Observable_class.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+// Adding Codable conformance to an @Observable class
+
+
+import SwiftUI
+
+@Observable
+class User: Codable {
+    // Fixing the coding key
+    // From "{"_name":"Cataleya","_$observationRegistrar":{}}"
+    // To {"name" : "Cataleya"}
+    // by replacing _name with "name"
+    enum CodingKeys : String, CodingKey {
+        case _name = "name"
+    }
+    var name = "Cataleya"
+}
+
+struct ContentView: View {
+    
+    var body: some View {
+        Button("Encode Cataleya", action:  encodeCataleya)
+    }
+    
+    func encodeCataleya() {
+        // data instance = try encoding a new User
+        let data = try! JSONEncoder().encode(User())
+        // str instance = displaying the data
+        let str = String(decoding: data, as: UTF8.self)
+        print(str)
+    }
+}
+
+#Preview {
+    ContentView()
+}
