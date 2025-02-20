be4cad36c5009edf02a534398f3ccb150c675235
JoshuaRosado <joshua06rosado@gmail.com>
2025-02-20 14:55:57 -0600

    Challenge 2 completed

--- starting_swiftUI/day-52(Challenge_CupcakeCorner/Challenege_CupcakeCorner/Challenege_CupcakeCorner.playground/Contents.swift
+++ starting_swiftUI/day-52(Challenge_CupcakeCorner/Challenege_CupcakeCorner/Challenege_CupcakeCorner.playground/Contents.swift
@@ -11,7 +11,7 @@
 //Our address fields are currently considered valid if they contain anything, even if it’s just only whitespace. Improve the validation to make sure a string of pure whitespace is invalid.
 
 
-// CHALLENGE 2 -
+// CHALLENGE 2 - COMPLETED
 //If our call to placeOrder() fails – for example if there is no internet connection – show an informative alert for the user. To test this, try commenting out the request.httpMethod = "POST" line in your code, which should force the request to fail.
 
 // CHALLENGE 3 -
