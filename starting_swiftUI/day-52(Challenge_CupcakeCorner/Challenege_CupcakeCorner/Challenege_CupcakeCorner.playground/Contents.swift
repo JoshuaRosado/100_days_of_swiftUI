a11df265c9246b32ebee11ef0feb9968350cfec2
JoshuaRosado <joshua06rosado@gmail.com>
2025-02-20 14:39:58 -0600

    Challenge 1 completed

--- starting_swiftUI/day-52(Challenge_CupcakeCorner/Challenege_CupcakeCorner/Challenege_CupcakeCorner.playground/Contents.swift
+++ starting_swiftUI/day-52(Challenge_CupcakeCorner/Challenege_CupcakeCorner/Challenege_CupcakeCorner.playground/Contents.swift
@@ -2,8 +2,18 @@
 
 
 //Challenge
+
+
 //One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try extending this app to make sure you fully understand what’s going on.
 //
+
+// CHALLENGE 1 - COMPLETED
 //Our address fields are currently considered valid if they contain anything, even if it’s just only whitespace. Improve the validation to make sure a string of pure whitespace is invalid.
+
+
+// CHALLENGE 2 -
 //If our call to placeOrder() fails – for example if there is no internet connection – show an informative alert for the user. To test this, try commenting out the request.httpMethod = "POST" line in your code, which should force the request to fail.
+
+// CHALLENGE 3 -
+
 //For a more challenging task, try updating the Order class so it saves data such as the user's delivery address to UserDefaults. This takes a little thinking, because @AppStorage won't work here, and you'll find getters and setters cause problems with Codable support. Can you find a middle ground?
