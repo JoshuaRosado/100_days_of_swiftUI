67c30c82e5a22b676e7edd23439b3ea51f17c7e6
JoshuaRosado <joshua06rosado@gmail.com>
2025-02-23 08:13:24 -0600

    Difference between property wrappers

---
+++ starting_swiftUI/day-53(BookWorm)/Creating_a_custom_component_with_@Binding/Creating_a_component_with_@Binding.playground/Contents.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+// Creating a component with @Binding
+
+
+
+
+// Property Wrappers
+//@State = LOCAL value types
+//@Bindable = Bindings to properties inside observable classes
+
+
+//@Binding = Allows us to share an @State that belong to one View with another View
+
+import SwiftUI
+
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white:0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        // If Bool (isOn) is true
+        // Display onColors
+        // If (isOn) is false
+        // Display offColors
+        .background(LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom))
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        .shadow(radius: isOn ? 0 : 5)
+        
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var rememberMe = false
+    
+    var body: some View {
+        VStack {
+            
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            // Using rememberMe with "$" because this parameters is used with @Binding
+            
+            
+            Text(rememberMe ? "On" : "Off")
+            
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
