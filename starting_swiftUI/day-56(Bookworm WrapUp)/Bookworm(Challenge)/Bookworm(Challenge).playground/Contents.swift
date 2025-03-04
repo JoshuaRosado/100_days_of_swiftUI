b07d748a6746388a2d92cdf9638b1fca33317a29
JoshuaRosado <joshua06rosado@gmail.com>
2025-03-04 16:03:10 -0600

    Challenge in progress

---
+++ starting_swiftUI/day-56(Bookworm WrapUp)/Bookworm(Challenge)/Bookworm(Challenge).playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// Challenge
+
+
+//One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try extending this app to make sure you fully understand what’s going on.
+//
+// 1 - Right now it’s possible to select no title, author, or genre for books, which causes a problem for the detail view. Please fix this, either by forcing defaults, validating the form, or showing a default picture for unknown genres you can choose.
+
+// 2 - Modify ContentView so that books rated as 1 star are highlighted somehow, such as having their name shown in red.
+
+// 3 -Add a new “date” attribute to the Book class, assigning Date.now to it so it gets the current date and time, then format that nicely somewhere in DetailView.
+
+
