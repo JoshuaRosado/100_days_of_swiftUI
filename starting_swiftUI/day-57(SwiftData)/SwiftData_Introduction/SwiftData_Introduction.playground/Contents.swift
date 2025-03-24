d34254b9025dc9fd8e694b8f99add8ef45d4c36c
JoshuaRosado <joshua06rosado@gmail.com>
2025-03-24 10:06:46 -0500

    Introduction to SwiftData

---
+++ starting_swiftUI/day-57(SwiftData)/SwiftData_Introduction/SwiftData_Introduction.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// SwiftData Introduction
+
+
+//This technique project is going to explore SwiftData in more detail, starting with a summary of some basic techniques then building up to tackling some more complex problems.
+//
+//As you'll see, SwiftData really pushes hard on advanced features of both Swift and SwiftUI, all to help make it easy for us to store data efficiently. It's not always easy, though, and there are a few places that take quite a bit of thinking to use properly.
+//
+//We have lots to explore, so please create a fresh project where we can try it out. Call it “SwiftDataProject” and not just “SwiftData” because that will cause Xcode to get confused.
+//
+//Make sure you do not enable SwiftData for storage. Again, we'll be building this from scratch so you can see how it all works.
+//
+//All set? Let’s go!
