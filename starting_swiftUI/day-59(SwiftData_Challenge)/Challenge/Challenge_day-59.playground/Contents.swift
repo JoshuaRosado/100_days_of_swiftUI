adb8ee47b1294d2873100c333e52dfb23a30836f
JoshuaRosado <joshua06rosado@gmail.com>
2025-05-01 15:25:08 -0500

    Challenge stage

---
+++ starting_swiftUI/day-59(SwiftData_Challenge)/Challenge/Challenge_day-59.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+//Challenge
+//One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try extending this app to make sure you fully understand what’s going on.
+//
+//All three of these challenges relate to you upgrade project 7, iExpense:
+//
+
+// Challenge 1
+//Start by upgrading it to use SwiftData.
+
+// Challenge 2
+//Add a customizable sort order option: by name or by amount.
+
+// Challenge 3
+//Add a filter option to show all expenses, just personal expenses, or just business expenses.
