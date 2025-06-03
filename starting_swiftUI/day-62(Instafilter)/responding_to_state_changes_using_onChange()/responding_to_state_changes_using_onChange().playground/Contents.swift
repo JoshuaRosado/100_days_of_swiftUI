18bb3f6b805ea544e659243b8f07fa9303afcaa9
JoshuaRosado <joshua06rosado@gmail.com>
2025-06-03 13:47:24 -0500

    onChange added to track each update from the state

---
+++ starting_swiftUI/day-62(Instafilter)/responding_to_state_changes_using_onChange()/responding_to_state_changes_using_onChange().playground/Contents.swift
@@ -0,0 +1,31 @@
+
+
+// Responding to state changes using onChange()
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var blurAmount = 0.0
+    
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .blur(radius: blurAmount)
+            //
+            Slider(value: $blurAmount, in : 0...20)
+            // When blurAmount updates,  return old and new value
+            // and print new value
+                .onChange(of: blurAmount) {oldValue, newValue in
+                    print("New value is \(newValue)")
+                }
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
