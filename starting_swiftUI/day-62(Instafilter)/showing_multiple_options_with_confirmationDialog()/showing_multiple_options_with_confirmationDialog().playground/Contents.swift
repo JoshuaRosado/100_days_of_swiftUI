70180ab4a1e0fbdb84fbfabcd6edadad2163ed01
JoshuaRosado <joshua06rosado@gmail.com>
2025-06-04 13:30:16 -0500

    Using confirmationDialog()

---
+++ starting_swiftUI/day-62(Instafilter)/showing_multiple_options_with_confirmationDialog()/showing_multiple_options_with_confirmationDialog().playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+import SwiftUI
+
+// Showing multiple options with confirmationDialog()
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+    
+    var body: some View {
+        Button("Hello, world"){
+            showingConfirmation.toggle()
+        }
+        .frame(width: 300, height: 300)
+        .background(backgroundColor)
+        // When the "var showingConfirmation" is triggered, open this confirmationDialog(small window with options)
+        .confirmationDialog("Change background", isPresented: $showingConfirmation){
+            
+            // All the options in the confirmationDialog()
+            Button("Red"){backgroundColor = .red}
+            Button("Green"){backgroundColor = .green}
+            Button("Blue"){backgroundColor = .blue}
+            // Always give a straight forward way for users to close this confirmationDialog()
+            Button("Cancel", role: .cancel){}
+
+            
+        } message: {
+            // Description
+            Text("Select a new color.")
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
