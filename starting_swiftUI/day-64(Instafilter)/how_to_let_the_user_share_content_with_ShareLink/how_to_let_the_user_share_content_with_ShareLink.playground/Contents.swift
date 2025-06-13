f461acad8c8706092a6f9b03d1c57ad24767986c
JoshuaRosado <joshua06rosado@gmail.com>
2025-06-13 16:27:21 -0500

    Sharing Links

---
+++ starting_swiftUI/day-64(Instafilter)/how_to_let_the_user_share_content_with_ShareLink/how_to_let_the_user_share_content_with_ShareLink.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+// How to let the User share content with ShareLink
+
+
+
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Joshua Rosado Olivencia on 5/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // Sharing image
+    let example = Image(.italy)
+    
+    var body: some View {
+        
+        // *Sharing URL
+        
+//        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!){
+//            Label("Spread the word about hacking with swift", systemImage: "swift")
+//        }
+        
+        
+        // *Sharing Image
+        
+        ShareLink(item: example, preview: SharePreview("Italy", image: example)){
+            Label("Share", systemImage: "airplane")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
