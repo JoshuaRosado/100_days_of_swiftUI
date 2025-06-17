95c0696f89d0b525491afa38f424eacbd1272039
JoshuaRosado <joshua06rosado@gmail.com>
2025-06-17 13:37:31 -0500

    Using requestReview

---
+++ starting_swiftUI/day-64(Instafilter)/how_to_ask_the_user_to_leave_an_AppStore_review/how_to_ask_the_user_to_leave_an_AppStore_review.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+// How to ask the user to leave an App Store review
+
+//  Created by Joshua Rosado Olivencia on 5/31/25.
+//
+
+// STEP 1
+import StoreKit //*
+import SwiftUI
+
+struct ContentView: View {
+    // STEP 2
+    // ADD Property to read the review requester from SwiftUI's environment
+    @Environment(\.requestReview) var requestReview
+    var body: some View {
+        Text("Instafilter")
+        
+        // STEP 3
+        // Need to request a review when ready
+        
+        Button("Leave a review"){
+            requestReview()
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
+
