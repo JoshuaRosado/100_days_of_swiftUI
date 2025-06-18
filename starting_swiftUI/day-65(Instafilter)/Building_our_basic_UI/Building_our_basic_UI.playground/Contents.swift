20ee53ce7c09a26725313b146c50d2faf6014e77
JoshuaRosado <joshua06rosado@gmail.com>
2025-06-18 13:35:43 -0500

    Building our basic UI

---
+++ starting_swiftUI/day-65(Instafilter)/Building_our_basic_UI/Building_our_basic_UI.playground/Contents.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+// Building our basic UI
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    var body: some View {
+        VStack{
+            Spacer()
+            //image area
+            if let processedImage {
+                processedImage
+                    .resizable()
+                    .scaledToFit()
+            } else {
+                ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+            }
+            Spacer()
+            
+            HStack{
+                Text("Intensity")
+                Slider(value:$filterIntensity)
+            }
+            
+            HStack{
+                Button("Change Filter", action: changeFilter)
+                    //change filter
+                
+                Spacer()
+                
+                    //share the picture
+            }
+            
+        }
+        .padding([.horizontal, .bottom])
+        .navigationTitle("Instafilter")
+        
+        
+        }
+    
+    func changeFilter() {
+        
+    }
+      
+}
+
+#Preview {
+    ContentView()
+}
