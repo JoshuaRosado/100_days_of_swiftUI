d66106165152aafd7dc9146fb72bc90cd5e7e842
JoshuaRosado <joshua06rosado@gmail.com>
2025-06-19 13:39:06 -0500

    Adding PhotosPicker to load image data

---
+++ starting_swiftUI/day-65(Instafilter)/Importing_an_image_into_SwiftUI_using_PhotosPicker/Importing_an_image_into_SwiftUI_using_PhotosPicker.playground/Contents.swift
@@ -0,0 +1,78 @@
+import UIKit
+
+
+// Importing an Image into SwiftUI using PhotosPicker
+
+
+import PhotosUI
+import SwiftUI
+
+struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
+    // Track every picture selected
+    // Optional, there isn't one by default
+    @State private var selectedItem: PhotosPickerItem?
+    
+    var body: some View {
+        VStack{
+            Spacer()
+            // Add a PhotosPicker view to trigger an image selection
+            //image area
+            PhotosPicker(selection: $selectedItem){
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Import a photo to get started"))
+                }
+            }
+            .buttonStyle(.plain)
+                Spacer()
+            
+            HStack{
+                Text("Intensity")
+                Slider(value:$filterIntensity)
+            }
+            
+            HStack{
+                Button("Change Filter", action: changeFilter)
+                    //change filter
+                
+                Spacer()
+                
+                    //share the picture
+            }
+            
+        }
+        .padding([.horizontal, .bottom])
+        .navigationTitle("Instafilter")
+        
+        
+        }
+    
+    func changeFilter() {
+        
+    }
+    
+    func loadImage() {
+        Task{
+            
+            // load data if there is one
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            
+            // insert imageData into a UIImage
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            
+        }
+    }
+      
+}
+
+#Preview {
+    ContentView()
+}
+
