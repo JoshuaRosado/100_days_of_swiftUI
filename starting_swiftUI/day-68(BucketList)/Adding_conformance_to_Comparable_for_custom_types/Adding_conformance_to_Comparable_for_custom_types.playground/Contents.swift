ad96246ac4feb115e91e73a7a032ea73a1cde98e
JoshuaRosado <joshua06rosado@gmail.com>
2025-09-01 14:49:06 -0500

    Adding conformance to Comparable for custom types

---
+++ starting_swiftUI/day-68(BucketList)/Adding_conformance_to_Comparable_for_custom_types/Adding_conformance_to_Comparable_for_custom_types.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+
+// Adding conformance to Comparable for custom types
+
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Joshua Rosado Olivencia on 8/31/25.
+//
+
+import SwiftUI
+
+struct User: Comparable, Identifiable{ // Add Comparable
+    let id = UUID()
+    let firstName:String
+    let lastName: String
+    
+    // lhs = left hand side
+    // rhs = right hand side
+    // < = operator over loaded
+    // static func = is called directly in the User struct itself
+    
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+    
+        // Now that our User struct conforms to Identifiable and Comparable, we automatically get accesss to the parameter-less version of sorted()
+}
+
+struct ContentView: View {
+    let users = [
+        User(firstName: " Khai", lastName: "Asher"),
+        User(firstName: " Cataleya", lastName: "Grace"),
+        User(firstName: " Alexis", lastName: "Elizabeth")
+    ] // .sorted {
+    //$0.lastName < $1.lastName
+    
+    var body: some View {
+        List(users){ user in
+            Text("\(user.lastName),\(user.firstName)")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
